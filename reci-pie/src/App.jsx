839aba35e14d8fa10060b18dc28841206331cda3
Nabeel Maklai <nabilmaklai@hotmail.com>
2024-03-21 11:35:48 +0300

    react app skeleton

---
+++ reci-pie/src/App.jsx
@@ -0,0 +1,10 @@
+import { useState } from 'react'
+// import { Routes, Route } from 'react-router-dom'
+
+import './App.css'
+
+const App = () => {
+  return <div>HELLO</div>
+}
+
+export default App
