924ec46d509a13b819629bc6d84a584525f59a02
Nabeel Maklai <nabilmaklai@hotmail.com>
2024-03-24 01:35:13 +0300

    show all recipes and delete ingredient page refresh fix

--- reci-pie/src/App.jsx
+++ reci-pie/src/App.jsx
@@ -1,17 +1,18 @@
-import { useState, useEffect } from "react"
-import { Routes, Route } from "react-router-dom"
+import { useState, useEffect } from 'react'
+import { Routes, Route } from 'react-router-dom'
 
 // import { CheckSession } from "./services/Auth"
 
-import Header from "./components/Header"
-import NavBar from "./components/NavBar"
-import Home from "./pages/Home"
-import Recipes from "./pages/Recipes"
-import Profile from "./pages/Profile"
-import ShoppingList from "./pages/ShoppingList"
-import RegistartionForm from "./pages/RegisterForm"
+import Header from './components/Header'
+import NavBar from './components/NavBar'
+import Home from './pages/Home'
+import Recipes from './pages/Recipes'
+import Profile from './pages/Profile'
+import ShoppingList from './pages/ShoppingList'
+import RegistartionForm from './pages/RegisterForm'
 import AddRecipe from './pages/AddRecipe'
-import "./styles/App.scss"
+import AllRecipes from './pages/AllRecipes'
+import './styles/App.scss'
 
 const App = () => {
   const [user, setUser] = useState(null)
@@ -28,7 +29,7 @@ const App = () => {
   }
 
   useEffect(() => {
-    const token = localStorage.getItem("token")
+    const token = localStorage.getItem('token')
     if (token) {
       checkToken()
     }
@@ -47,7 +48,7 @@ const App = () => {
           <Route path="/recipes" element={<Recipes />} />
           <Route path="/shopping-list" element={<ShoppingList />} />
           <Route path="/addrecipe" element={<AddRecipe />} />
-
+          <Route path="/allrecipes" element={<AllRecipes />} />
         </Routes>
       </div>
     </div>
