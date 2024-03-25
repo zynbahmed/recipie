3eb9f0c380f43b8e04b2320091cfbb82ccc9e3fe
Nayef Alzayani <nzayani57@outlook.com>
2024-03-25 22:37:50 +0300

    saved recipes

--- reci-pie/src/components/SavedRecipe.jsx
+++ reci-pie/src/components/SavedRecipe.jsx
@@ -2,21 +2,19 @@ import Client from '../services/api'
 import { useState, useEffect } from 'react'
 import axios from 'axios'
 const SavedRecipe = ({ user }) => {
-  const [recipe, setRecipe] = useState({})
-
-  useEffect(() => {
-    getRecipe()
-  }, [])
-
-  const getRecipe = async () => {
-    const response = await Client.get(`/recipe`)
-  }
-
+  const recipes = user?.savedRecipes
+  console.log(recipes)
   return (
     <div>
       <h1 className="text-xl font-bold mb-4">Saved Recipes</h1>
-
-      <h2>{}</h2>
+      <div>
+        {recipes.map((recipe) => (
+          <div>
+            <h1>{recipe.title}</h1>
+            <img src={recipe.photo} alt="" />
+          </div>
+        ))}
+      </div>
     </div>
   )
 }
