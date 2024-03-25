545c05a6b00719cc8240b0149fde39c502eb4cb2
zynbahmed <201901241@student.polytechnic.bh>
2024-03-25 13:49:45 +0300

    fixed the stars in the review

--- reci-pie/src/components/Reviews.jsx
+++ reci-pie/src/components/Reviews.jsx
@@ -28,7 +28,7 @@ const Reviews = ({ reviews }) => {
                 </span>
                 {review.content}
               </p>
-              <ul class="mb-0 flex items-center justify-center">
+              {/* <ul class="mb-0 flex items-center justify-center">
                 <li>
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
@@ -102,7 +102,49 @@ const Reviews = ({ reviews }) => {
                     />
                   </svg>
                 </li>
-              </ul>
+              </ul> */}
+              <div class="flex space-x-2 mt-4 items-center justify-center">
+              <svg
+                class="w-5 fill-red-800"
+                viewBox="0 0 14 13"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path d="M7 0L9.4687 3.60213L13.6574 4.83688L10.9944 8.29787L11.1145 12.6631L7 11.2L2.8855 12.6631L3.00556 8.29787L0.342604 4.83688L4.5313 3.60213L7 0Z" />
+              </svg>
+              <svg
+                class="w-5 fill-red-800"
+                viewBox="0 0 14 13"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path d="M7 0L9.4687 3.60213L13.6574 4.83688L10.9944 8.29787L11.1145 12.6631L7 11.2L2.8855 12.6631L3.00556 8.29787L0.342604 4.83688L4.5313 3.60213L7 0Z" />
+              </svg>
+              <svg
+                class="w-5 fill-red-800"
+                viewBox="0 0 14 13"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path d="M7 0L9.4687 3.60213L13.6574 4.83688L10.9944 8.29787L11.1145 12.6631L7 11.2L2.8855 12.6631L3.00556 8.29787L0.342604 4.83688L4.5313 3.60213L7 0Z" />
+              </svg>
+              <svg
+                class="w-5 fill-red-800"
+                viewBox="0 0 14 13"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path d="M7 0L9.4687 3.60213L13.6574 4.83688L10.9944 8.29787L11.1145 12.6631L7 11.2L2.8855 12.6631L3.00556 8.29787L0.342604 4.83688L4.5313 3.60213L7 0Z" />
+              </svg>
+              <svg
+                class="w-5 fill-[#CED5D8]"
+                viewBox="0 0 14 13"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path d="M7 0L9.4687 3.60213L13.6574 4.83688L10.9944 8.29787L11.1145 12.6631L7 11.2L2.8855 12.6631L3.00556 8.29787L0.342604 4.83688L4.5313 3.60213L7 0Z" />
+              </svg>
+            </div>
               {/* <h3 className=""> {review.rating}</h3> */}
               {/* <hr /> */}
             </div>
