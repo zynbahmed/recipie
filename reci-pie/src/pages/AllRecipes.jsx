23a7b120f61cd0ba54392db266cb005bcd3e7548
zynbahmed <201901241@student.polytechnic.bh>
2024-03-26 02:34:20 +0300

    desiging the categories

--- reci-pie/src/pages/AllRecipes.jsx
+++ reci-pie/src/pages/AllRecipes.jsx
@@ -1,14 +1,14 @@
-import { useEffect, useState } from 'react'
-import { useNavigate } from 'react-router-dom'
-import Client from '../services/api'
-import RecipeCard from '../components/RecipeCard'
+import { useEffect, useState } from "react"
+import { useNavigate } from "react-router-dom"
+import Client from "../services/api"
+import RecipeCard from "../components/RecipeCard"
 const AllRecipes = () => {
   let navigate = useNavigate()
   const [allRecipes, setAllRecipes] = useState([])
-  const [cat, setCat] = useState('')
+  const [cat, setCat] = useState("")
   useEffect(() => {
     const getAllRecipes = async () => {
-      const response = await Client.get('/recipe/recipesbycat', {
+      const response = await Client.get("/recipe/recipesbycat", {
         params: { cat: cat }
       })
 
@@ -20,7 +20,7 @@ const AllRecipes = () => {
   }, [cat])
 
   const adding = () => {
-    navigate('/addrecipe')
+    navigate("/addrecipe")
   }
   const handleSelect = (selected) => {
     setCat(selected)
@@ -28,45 +28,63 @@ const AllRecipes = () => {
 
   return (
     <div className="max-w-screen-xl mx-auto p-5 sm:p-10 md:p-16">
-      <button
-        onClick={() => {
-          handleSelect('Beef')
-        }}
-      >
-        <div>Beef</div>
-      </button>
-      <button
-        onClick={() => {
-          handleSelect('Lamb')
-        }}
-      >
-        <div>Lamb</div>
-      </button>
-      <button
-        onClick={() => {
-          handleSelect('Chicken')
-        }}
-      >
-        <div>Chicken</div>
-      </button>
-      <button
-        onClick={() => {
-          handleSelect('Sea-food')
-        }}
-      >
-        <div>Sea-food</div>
-      </button>
-      <button
-        onClick={() => {
-          handleSelect('Dessert')
-        }}
-      >
-        <div>Dessert</div>
-      </button>
-
+      {/* <select onChange={handleSelect}>
+        <option value="Beef">Beef</option>
+        <option value="Lamb">Lamb</option>
+        <option value="Sea-food">Seafood</option>
+        <option value="Chicken">Chicken</option>
+      </select> */}
       <button className="reg-btn m-2" onClick={adding}>
         Add Recipe
       </button>
+      <div className="grid gap-6 text-center md:grid-cols-6 lg:gap-12 m-2 mb-10">
+        <div>
+          <button
+            onClick={() => {
+              handleSelect("Beef")
+            }}
+          >
+            Beef
+          </button>
+        </div>
+        <div>
+          <button
+            onClick={() => {
+              handleSelect("Lamb")
+            }}
+          >
+            Lamb
+          </button>
+        </div>
+        <div>
+          <button
+            onClick={() => {
+              handleSelect("Chicken")
+            }}
+          >
+            Chicken
+          </button>
+        </div>
+        <div>
+          <button
+            onClick={() => {
+              handleSelect("Sea-food")
+            }}
+          >
+            Sea-food
+          </button>
+        </div>
+        <div>
+          <button
+            onClick={() => {
+              handleSelect("Dessert")
+            }}
+          >
+            Dessert
+          </button>
+        </div>
+      </div>
+
       <RecipeCard allRecipes={allRecipes} />
     </div>
   )
