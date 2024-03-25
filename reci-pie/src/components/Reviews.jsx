67ffa1c3593bd675b7102b1bce3a81a3b33b0615
zynbahmed <201901241@student.polytechnic.bh>
2024-03-25 13:44:07 +0300

    review fixed + desiging the view reviews

--- reci-pie/src/components/Reviews.jsx
+++ reci-pie/src/components/Reviews.jsx
@@ -1,19 +1,118 @@
 const Reviews = ({ reviews }) => {
-    return reviews && reviews.length > 0 ? (
-      <div>
-        <h3 className="">Reviews</h3>
-        {reviews.map((review) => ( 
-          <div className="" key={review._id}>
-            <h3 className=""> {review.rating}</h3>
-          <h3 className=""> {review.content}</h3>
-        <hr/>
-        </div>
-        ))}
+  return reviews && reviews.length > 0 ? (
+    <div>
+      <div class="mx-auto text-center md:max-w-xl lg:max-w-3xl">
+        <h3 class="mb-6 text-3xl font-bold">Reviews</h3>
       </div>
-      
-    ) : (
-      <p>No reviews available</p>
-    )
-  }
-  export default Reviews
-  
+      <div class="max-w-screen-xl mx-auto p-5 sm:p-10 md:p-16">
+        <div class="grid gap-6 text-center md:grid-cols-3 lg:gap-12">
+          {reviews.map((review) => (
+            <div className="" key={review._id}>
+              <div class="mb-6 flex justify-center">
+                <img
+                  className=""
+                  src={review.userAvatar}
+                  class="w-32 rounded-full shadow-lg dark:shadow-black/30"
+                />
+              </div>
+              <h5 class="mb-4 text-xl font-semibold">{review.userName}</h5>
+              <p class="mb-4 text-neutral-600 dark:text-neutral-300">
+                <span class="inline-block pe-2 [&>svg]:w-5">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="currentColor"
+                    viewBox="0 0 448 512"
+                  >
+                    <path d="M0 216C0 149.7 53.7 96 120 96h8c17.7 0 32 14.3 32 32s-14.3 32-32 32h-8c-30.9 0-56 25.1-56 56v8h64c35.3 0 64 28.7 64 64v64c0 35.3-28.7 64-64 64H64c-35.3 0-64-28.7-64-64V320 288 216zm256 0c0-66.3 53.7-120 120-120h8c17.7 0 32 14.3 32 32s-14.3 32-32 32h-8c-30.9 0-56 25.1-56 56v8h64c35.3 0 64 28.7 64 64v64c0 35.3-28.7 64-64 64H320c-35.3 0-64-28.7-64-64V320 288 216z" />
+                  </svg>
+                </span>
+                {review.content}
+              </p>
+              <ul class="mb-0 flex items-center justify-center">
+                <li>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 24 24"
+                    fill="currentColor"
+                    class="h-5 w-5 text-red-500"
+                  >
+                    <path
+                      fill-rule="evenodd"
+                      d="M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.007 5.404.433c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.433 2.082-5.006z"
+                      clip-rule="evenodd"
+                    />
+                  </svg>
+                </li>
+                <li>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 24 24"
+                    fill="currentColor"
+                    class="h-5 w-5 text-red-500"
+                  >
+                    <path
+                      fill-rule="evenodd"
+                      d="M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.007 5.404.433c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.433 2.082-5.006z"
+                      clip-rule="evenodd"
+                    />
+                  </svg>
+                </li>
+                <li>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 24 24"
+                    fill="currentColor"
+                    class="h-5 w-5 text-red-500"
+                  >
+                    <path
+                      fill-rule="evenodd"
+                      d="M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.007 5.404.433c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.433 2.082-5.006z"
+                      clip-rule="evenodd"
+                    />
+                  </svg>
+                </li>
+                <li>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 24 24"
+                    fill="currentColor"
+                    class="h-5 w-5 text-red-500"
+                  >
+                    <path
+                      fill-rule="evenodd"
+                      d="M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.007 5.404.433c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.433 2.082-5.006z"
+                      clip-rule="evenodd"
+                    />
+                  </svg>
+                </li>
+                <li>
+                  <svg
+                    clip-rule="evenodd"
+                    fill-rule="evenodd"
+                    fill="currentColor"
+                    class="h-5 w-5 text-red-500"
+                    stroke-linejoin="round"
+                    stroke-miterlimit="2"
+                    viewBox="0 0 24 24"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      d="m11.322 2.923c.126-.259.39-.423.678-.423.289 0 .552.164.678.423.974 1.998 2.65 5.44 2.65 5.44s3.811.524 6.022.829c.403.055.65.396.65.747 0 .19-.072.383-.231.536-1.61 1.538-4.382 4.191-4.382 4.191s.677 3.767 1.069 5.952c.083.462-.275.882-.742.882-.122 0-.244-.029-.355-.089-1.968-1.048-5.359-2.851-5.359-2.851s-3.391 1.803-5.359 2.851c-.111.06-.234.089-.356.089-.465 0-.825-.421-.741-.882.393-2.185 1.07-5.952 1.07-5.952s-2.773-2.653-4.382-4.191c-.16-.153-.232-.346-.232-.535 0-.352.249-.694.651-.748 2.211-.305 6.021-.829 6.021-.829s1.677-3.442 2.65-5.44zm.678 2.033v11.904l4.707 2.505-.951-5.236 3.851-3.662-5.314-.756z"
+                      fill-rule="nonzero"
+                    />
+                  </svg>
+                </li>
+              </ul>
+              {/* <h3 className=""> {review.rating}</h3> */}
+              {/* <hr /> */}
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  ) : (
+    // </div>
+    <p>No reviews available</p>
+  )
+}
+export default Reviews
