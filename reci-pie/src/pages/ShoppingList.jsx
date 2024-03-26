c8117a5e6bdce4f75e462c5748abd1e4b66148c4
zynbahmed <201901241@student.polytechnic.bh>
2024-03-26 23:14:26 +0300

    shopping list

--- reci-pie/src/pages/ShoppingList.jsx
+++ reci-pie/src/pages/ShoppingList.jsx
@@ -1,19 +1,46 @@
-import Client from '../services/api'
+import Client from "../services/api"
+import { useState, useEffect } from "react"
 const ShoppingList = ({ list, user, setList }) => {
+  const flattenedList = list.flat()
   const add = async () => {
-    const flattenedList = list.flat()
     const request = { flattenedList, user }
-    await Client.post('/list', request)
+    await Client.post("/list", request)
     setList([])
   }
+
+  const [shList, setShList] = useState([])
+
+  useEffect(() => {
+    const details = async () => {
+      let selected = await Client.get("/")
+      setShList(selected.data.shoppingList)
+    }
+    details()
+  }, [shList])
+
+  
+
   return (
-    <div>
-      <h1>Shopping List</h1>
-      {list.map((item) => (
-        <p>{item}</p>
-      ))}
-      <button onClick={add}>Add to Shopping List</button>
+    <div className="max-w-screen-xl mx-auto p-5 sm:p-10 md:p-16">
+      <h1 className="text-xl font-bold mb-4">Shopping List</h1>
+      <ul className="space-y-3 mt-4 pl-4 text-sm list-disc">
+        {flattenedList.map((item) => (
+          <li className="mr-4">
+            <p>{item}</p>
+          </li>
+        ))}
+      </ul>
+      <button className="reg-btn m-2" onClick={add}>
+        Add to Shopping List
+      </button>
       <h2>Groceries</h2>
+      <ul className="space-y-3 mt-4 pl-4 text-sm list-disc">
+        {shList?.map((item) => (
+          <li className="mr-4">
+            <p>{item.name}</p>
+          </li>
+        ))}
+      </ul>
     </div>
   )
 }
