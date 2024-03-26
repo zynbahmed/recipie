31f5a69a4091b5b9a28a41ee21b565bf757940b6
Nabeel Maklai <nabilmaklai@hotmail.com>
2024-03-26 13:13:50 +0300

    additin of shopping list page and selecting grocery items

--- reci-pie/src/pages/ShoppingList.jsx
+++ reci-pie/src/pages/ShoppingList.jsx
@@ -1,7 +1,11 @@
-const ShoppingList = () => {
+const ShoppingList = ({ list }) => {
   return (
     <div>
       <h1>Shopping List</h1>
+      {list.map((item) => (
+        <p>{item}</p>
+      ))}
+
       <h2>Groceries</h2>
     </div>
   )
