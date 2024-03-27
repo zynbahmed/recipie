588fc67bf640ca2646eaaeea9dba652c2ce2e1dd
zynbahmed <201901241@student.polytechnic.bh>
2024-03-27 22:08:24 +0300

    buttons in recipe details changed

--- reci-pie/src/pages/RecipeDetails.jsx
+++ reci-pie/src/pages/RecipeDetails.jsx
@@ -168,44 +168,53 @@ const RecipeDetails = ({ user, list, setList }) => {
           <div className="grid items-start grid-cols-1 lg:grid-cols-5 gap-12 border border-grey-200 p-10 mb-5 shadow-xl">
             <div className="lg:col-span-3 bg-base-100 w-full top-0 text-center p-8">
               <img src={recipe?.photo} alt={recipe?.title} />
-              {user && user?.id === recipe?.creator?._id && (
-                <div class="flex flex-wrap gap-4 mt-4">
-                  <button
-                    className="text-red-500 font-extrabold uppercase hover:text-red-900"
-                    onClick={() => {
-                      handleDelete(recipe._id)
-                    }}
-                  >
-                    Delete
-                  </button>
-                  <button
-                    onClick={handleEditPage}
-                    className="text-blue-500 font-extrabold uppercase hover:text-blue-900"
-                  >
-                    Edit
-                  </button>
-                </div>
-              )}
-              {/* <div className="flex flex-wrap gap-4 mt-4">
-              <button
-                onClick={saveRecipe}
-                className="text-red-500 font-extrabold uppercase hover:text-green-900"
-              >
-                UnSave Recipe
-              </button>
-              <button
-                onClick={unsaveRecipe}
-                className="text-green-500 font-extrabold uppercase hover:text-green-900"
-              >
-                Unsave
-              </button>
-            </div> */}
-              <SaveRecipeButton
-                saved={saved}
-                saveRecipe={saveRecipe}
-                unsaveRecipe={unsaveRecipe}
-                user={user}
-              />
+              <div class="flex flex-wrap gap-4 mt-4">
+                {user && user?.id === recipe?.creator?._id && (
+                  <>
+                    <button
+                      className="btn hover:bg-red-500"
+                      onClick={() => {
+                        handleDelete(recipe._id)
+                      }}
+                    >
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="16"
+                        height="16"
+                        fill="currentColor"
+                        class="bi bi-trash"
+                        viewBox="0 0 16 16"
+                      >
+                        <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0z" />
+                        <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4zM2.5 3h11V2h-11z" />
+                      </svg>
+                      Delete
+                    </button>
+                    <button
+                      onClick={handleEditPage}
+                      className="btn hover:bg-blue-500"
+                    >
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="16"
+                        height="16"
+                        fill="currentColor"
+                        class="bi bi-pen"
+                        viewBox="0 0 16 16"
+                      >
+                        <path d="m13.498.795.149-.149a1.207 1.207 0 1 1 1.707 1.708l-.149.148a1.5 1.5 0 0 1-.059 2.059L4.854 14.854a.5.5 0 0 1-.233.131l-4 1a.5.5 0 0 1-.606-.606l1-4a.5.5 0 0 1 .131-.232l9.642-9.642a.5.5 0 0 0-.642.056L6.854 4.854a.5.5 0 1 1-.708-.708L9.44.854A1.5 1.5 0 0 1 11.5.796a1.5 1.5 0 0 1 1.998-.001m-.644.766a.5.5 0 0 0-.707 0L1.95 11.756l-.764 3.057 3.057-.764L14.44 3.854a.5.5 0 0 0 0-.708z" />
+                      </svg>
+                      Edit
+                    </button>
+                  </>
+                )}
+                <SaveRecipeButton
+                  saved={saved}
+                  saveRecipe={saveRecipe}
+                  unsaveRecipe={unsaveRecipe}
+                  user={user}
+                />
+              </div>
             </div>
             <div className="lg:col-span-2">
               <div className="flex flex-wrap gap-4 mt-4">
@@ -234,14 +243,18 @@ const RecipeDetails = ({ user, list, setList }) => {
                     </li>
                   ))}
                 </ul>
-                <button
-                  onClick={() => {
-                    addToCart(shoppingList)
-                  }}
-                  className="m-2 mt-5 reg-btn "
-                >
-                  Add to Grocery List
-                </button>
+                {shoppingList && shoppingList.length > 0 && (
+                  <>
+                    <button
+                      onClick={() => {
+                        addToCart(shoppingList)
+                      }}
+                      className="m-2 mt-5 reg-btn "
+                    >
+                      Add to Grocery List
+                    </button>
+                  </>
+                )}
               </div>
               <div className="mt-8">
                 <h3 className="text-lg font-bold">Step by Step</h3>
